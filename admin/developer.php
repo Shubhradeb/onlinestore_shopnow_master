62f16a7556bbfe0bfdd0aab8c128d59915ea1488
Shubhradeb <33101727+Shubhradeb@users.noreply.github.com>
2020-01-30 22:45:15 +0530

    Add files via upload

---
+++ admin/developer.php
@@ -0,0 +1,18 @@
+<h3><a href='../index.php' style='color:blue; text-decoration:none'>Back</a></h3><br><br>
+<h3 style='color:blue' align='center'>Developer's Profile</h3>
+<table border='1px solid black' align='center' cellspacing='0px' cellpadding='10px'>
+<tr>
+<th>Name</th>
+<th>Department</th>
+<th>Skills</th>
+<th>Contact No</th>
+<th>Email Id</th>
+</tr>
+<tr>
+<td>Sohel Islam Mollick</td>
+<td>Information Technology</td>
+<td>C, C++, Java, Php</td>
+<td>8697609789</td>
+<td>sohelislammollick@gmail.com</td>
+</tr>
+</table>
